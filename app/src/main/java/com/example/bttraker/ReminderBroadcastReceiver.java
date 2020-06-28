b7f577688af35c56085bc970b6bd02081f2bcfe3
2020m025 <283hisatuba@gmail.com>
2020-06-28 15:27:24 +0900

    Task5 Homework 5-3

--- app/src/main/java/com/example/bttraker/ReminderBroadcastReceiver.java
+++ app/src/main/java/com/example/bttraker/ReminderBroadcastReceiver.java
@@ -1,6 +1,7 @@
 package com.example.bttraker;
 
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -14,6 +15,10 @@ public class ReminderBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
+        // Create an Intent with LogActivity as the destination
+        Intent notificationIntent = new Intent(context,LogActivity.class);
+        //Create an PendingIntent with the newly created Intent
+        PendingIntent contentIntent  = PendingIntent.getActivity(context,0,notificationIntent,0);
         // An Intent broadcast.
         NotificationCompat.Builder builder = new
                 NotificationCompat.Builder(context ,"BT_Tracker_Channel")
@@ -21,6 +26,8 @@ public class ReminderBroadcastReceiver extends BroadcastReceiver {
                 .setContentTitle("Notification from BT Tracker")
                 .setContentText("Please log your body temperature now")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setContentIntent(contentIntent);//Append the PendingIntent when building notification
+
           NotificationManagerCompat notificationManager=
                   NotificationManagerCompat.from(context);
               notificationManager.notify(200,builder.build());
